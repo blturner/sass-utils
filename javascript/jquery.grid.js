3275d1b20755247dd92ecc59a115a8beca061cf0
Benjamin Turner <benturn@gmail.com>
2011-05-29 14:15:32 -0400

    changes defaults for grid and changes grid_col_gutter to gridColGutter

--- javascript/jquery.grid.js
+++ javascript/jquery.grid.js
@@ -3,10 +3,9 @@
     var defaults = {
       background: null,
       columns: 12,
-      grid_col_gutter: "0px"
+      gridColGutter: "5px"
     };
     var options = $.extend(defaults, options);
-    
     
     function calculateSize(elem) {
       var container = $(elem).find(".overlay").parent(),
@@ -19,25 +18,21 @@
         .css("height", height + "px");
     }
     
-    
     function columnWidth(n, context) {
       var width = n * (100 / context);
       return width + "%";
     }
-    
     
     return this.each(function() {
       // `obj` is the root +grid_container mixin element or a +row mixin element.
       var obj = $(this),
           overlay = $('<div class="overlay"></div>');
       
-      
       // Build the grid.
       for (i=0; i<options.columns; i++) {
         $('<span class="unit"><div class="gutter"></div></span>')
           .prependTo(overlay);
       }
-      
       
       obj.each(function() {
         var row = $(this),
@@ -50,12 +45,10 @@
           .clone().appendTo(row);
       });
       
-      
       $(obj).find(".unit")
         .css("width", columnWidth(1, options.columns))
-        .css("border-left", "solid " + options.grid_col_gutter + " rgba(255,255,255,0.15)")
-        .css("border-right", "solid " + options.grid_col_gutter + " rgba(255,255,255,0.15)");
-      
+        .css("border-left", "solid " + options.gridColGutter + " rgba(255,255,255,0.15)")
+        .css("border-right", "solid " + options.gridColGutter + " rgba(255,255,255,0.15)");
       
       // Recalculate grid overlay widths as the window resizes.
       $(window).resize(function() {
