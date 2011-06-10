3888ffb2fa329e41f270217bba313aee76f73f7d
Benjamin Turner <benturn@gmail.com>
2011-06-10 01:08:06 -0400

    toggling the toolbar works now

--- javascript/toolbar.js
+++ javascript/toolbar.js
@@ -8,16 +8,23 @@ var gridToolbar = window.gridToolbar || {};
   // var defaults = this.gridDefaults;
   
   this.init = function() {
-    var gridForm = ich.gridForm();
+    var gridForm = ich.gridForm(),
+        closeButton = '<a id="close" href="#">Close</a>';
     
     $("body").append(gridForm);
     $("#grid_toolbar form").bind("submit", this.addOrUpdateSelector);
+    
+    $('#grid_toolbar').prepend(closeButton);
+    $('#close').click(function() {
+      that.toggleToolbar();
+    });
     
     // setup any pre-defined grids
     if (window.grids) {
       $.each(window.grids, function(selector, options) {
         if (selector != "defaults") {
           $(selector).grid(options);
+          that.updateStorage(selector);
           
           var options = {
             "columns": window.grids[selector]["columns"],
@@ -30,6 +37,48 @@ var gridToolbar = window.gridToolbar || {};
   };
   
   
+  this.toggleToolbar = function() {
+    var toolbar = $('#grid_toolbar'),
+        toolbarWidth = toolbar.outerWidth();
+    
+    if (toolbar.hasClass('collapsed')) {
+      // Open the drawer
+      
+      toolbar.animate({
+        right: '+=' + toolbarWidth + 'px'
+      }, 250, function() {
+        $(this).removeClass('collapsed');
+        $('#close').removeClass('collapsed');
+      });
+      
+    } else {
+      // Close the drawer
+      
+      toolbar.animate({
+        right: '-=' + toolbarWidth + 'px'
+      }, 250, function() {
+        $(this).addClass('collapsed');
+        $('#close').addClass('collapsed');
+      });
+      
+    }
+  };
+  
+  
+  this.updateStorage = function(selector) {
+    if (!gridStorage[selector]) {
+      gridStorage[selector] = true;
+    }
+  };
+  
+  
+  this.removeStorage = function(selector) {
+    if (gridStorage[selector]) {
+      delete gridStorage[selector];
+    }
+  };
+  
+  
   this.addOrUpdateSelector = function() {
     var data = $(this).serializeArray(),
         options = {};
@@ -38,18 +87,22 @@ var gridToolbar = window.gridToolbar || {};
       options[data[i].name] = val.value;
     });
     
-    var selector = $(options["selector"]);
-    selector.grid(options);
+    var selector = options["selector"];
     
-    if (!gridStorage["selector"]) {
-      that.updateSelectorList(selector, options);
+    if (!gridStorage[selector]) {
+      $(selector).grid(options);
+      that.updateStorage(selector);
+      that.updateSelectorList($(selector), options);
+    } else {
+      $(selector).find("> .overlay").remove().end()
+        .grid(options);
     }
     
     return false;
-  }
+  };
   
   
-  this.removeSelector = function(event) {
+  this.removeSelector = function(event, selector) {
     var elem = event.target,
         item = $(elem).parent(),
         selector = $(item).find("code").text();
@@ -80,8 +133,8 @@ var gridToolbar = window.gridToolbar || {};
   
   
   this.toggleGrid = function(event) {
-    var elem = $(event.target),
-        parent = elem.parent(),
+    var elem = event.target,
+        parent = $(elem).parent(),
         selector = parent.find("code").text(),
         overlay = $(selector).find("> .overlay"),
         options = {
@@ -89,15 +142,14 @@ var gridToolbar = window.gridToolbar || {};
           "gridColGutter": parent.find('input[name="gridColGutter"]').val()
         };
     
-    if (!gridStorage[selector]) {
-      gridStorage[selector] = true;
+    if (gridStorage[selector]) {
       overlay.remove();
-      $(elem[0]).attr("checked", "")
+      that.removeStorage(selector);
+      // elem.removeAttr("checked");
     } else {
       $(selector).grid(options);
-      // $(selector).append(gridStorage[selector]);
-      delete gridStorage[selector];
-      $(elem[0]).attr("checked", "checked");
+      that.updateStorage(selector);
+      // elem.attr("checked", "checked");
     }
     
     return false;
